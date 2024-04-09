23b8a71261f9aa0e3b38c6b1d8e61aeeb16b42c7
kimjihoon <nagcha80@gmail.com>
2024-04-09 17:28:56 +0900

    gang

---
+++ for.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, o;
+
+    printf("홀수를 입력하세요.");
+    scanf_s("%d", &o);
+
+    if (o % 2 == 1) {
+        for (i = 0; i < o; i++)
+        {
+            for (j = 0; j < i; j++)
+                printf(" ");
+            for (j = i; j < o; j++)
+                printf("*");
+            printf("\n");
+        }
+    }
+    else {
+        printf("입력 오류.");
+    }
+    return 0;
+}
